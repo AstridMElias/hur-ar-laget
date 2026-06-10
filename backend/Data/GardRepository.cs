78aece3502ba7a43d24fa4d3b36e699b0f3d903f
Astrid Elias <astridm.elias@gmail.com>
2026-06-10 11:43:22 +0200

    lagt till Data/GardRepository

---
+++ backend/Data/GardRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic; // För att använda listor
+using HurArLaget.backend.Models; 
+
+namespace HurArLaget.backend.Data
+{
+    public class GardRepository // funktioner - Hämta och spara gård
+    {
+        public List<GardStatus> HämtaAllaGardar() // öppen,vad den gör, vad den heter
+        {
+            List<GardStatus> HämtaAllaGardar = new List<GardStatus>();
+
+            return listan;
+        }
+    }
+}
